82eab250bd0aa6aaa4122c716030efa04e9d04b5
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2024-04-07 16:09:09 +0200

    Silver layer airbnb draft

--- src/revo/data_processor/data_processor.py
+++ src/revo/data_processor/data_processor.py
@@ -1,6 +1,7 @@
 import logging
 
-from pyspark.sql import DataFrame, functions
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, regexp_replace, upper
 
 logger = logging.getLogger(__name__)
 
@@ -16,8 +17,18 @@ class DataProcessor:
     def clean_airbnb(self, df: DataFrame) -> DataFrame:
         """Generate the silver layer for AirBnB data."""
         try:
-            self.airbnb = df
-            return None
+            cols_changes = {
+                "zipcode": regexp_replace(upper(col("zipcode")), "\\s+", ""),
+                "latitude": col("latitude").cast("double"),
+                "longitude": col("longitude").cast("double"),
+                "accommodates": col("accommodates").cast("int"),
+                "bedrooms": col("bedrooms").cast("int"),
+                "price": col("price").cast("double"),
+                "review_scores_value": col("review_scores_value").cast("int"),
+            }
+
+            self.airbnb = df.dropDuplicates().withColumns(cols_changes)
+            return self.airbnb.copy()
         except Exception as e:
             logger.error(f"Error processing AirBnB data: {e}", exc_info=True)
             raise
@@ -38,6 +49,7 @@ class DataProcessor:
         """
 
         try:
+            # df.filter(df.room_type == "Entire home/apt")
             self.output = self.airbnb
             logger.info("Gold data processed successfully.")
             return self.output.copy()
