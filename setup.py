a59eaf04f9a6eae252b96a1f190c083eb9bbb555
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2024-04-07 13:13:36 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="revo",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    version="0.0.1",
+    description="RevoData assignment package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="gvalentini85",
+)
