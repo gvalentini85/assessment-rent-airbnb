77347dec4a9aaee3d1db84ddd4ff82e4c1503b63
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2024-04-08 18:30:59 +0200

    Initial draft for gold layer

--- src/revo/data_processor/data_processor.py
+++ src/revo/data_processor/data_processor.py
@@ -1,7 +1,7 @@
 import logging
 
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, lit, regexp_replace, upper
+from pyspark.sql.functions import col, lit, mean, regexp_replace, upper, when
 
 logger = logging.getLogger(__name__)
 
@@ -101,13 +101,92 @@ class DataProcessor:
 
     def aggregate_data(self) -> DataFrame:
         """
-        Aggregate the silver DataFrame to generate the gold layer, focusing on
-        key metrics.
+        Aggregate the silver DataFrames of AirBnB and Kamernet to generate
+        the gold layer with price metrics.
         """
 
         try:
-            # df.filter(df.room_type == "Entire home/apt")
-            self.output = self.airbnb
+            cols_list = [
+                "source",
+                "zipcode",
+                "daily_price",
+                "monthly_price",
+                "monthly_price_per_person",
+            ]
+            common_zipcodes = [
+                row["zipcode"]
+                for row in (
+                    self.airbnb.select("zipcode")
+                    .intersect(self.rentals.select("zipcode"))
+                    .collect()
+                )
+            ]
+
+            # Work on airbnb data:
+            # Filter only entire home
+            df_airbnb = self.airbnb.filter(col("type") == "Entire home/apt")
+
+            # Filter common zipcodes
+            df_airbnb = df_airbnb.filter(
+                df_airbnb.zipcode.isin(common_zipcodes)
+            )
+
+            # Set capacity > 5 to 6 (i.e., group all capacity higher
+            # than 5 together in a single category '6')
+            df_airbnb = df_airbnb.withColumn(
+                "capacity",
+                when(col("capacity") > 5, 6).otherwise(col("capacity")),
+            )
+
+            # Compute normalized measures
+            df_airbnb = (
+                df_airbnb.withColumn(
+                    "monthly_price", df_airbnb.price * 365.0 / 12.0
+                )
+                .withColumn(
+                    "monthly_price_per_person",
+                    df_airbnb.price * 365.0 / (12.0 * df_airbnb.capacity),
+                )
+                .withColumnRenamed("price", "daily_price")
+                .select([cname for cname in cols_list])
+            )
+
+            # Work on rentals data:
+            # Filter only entire apartment and studio
+            df_rentals = self.rentals.filter(
+                col("type").isin(["Apartment", "Studio"])
+            )
+
+            # Filter common zipcodes
+            df_rentals = df_rentals.filter(
+                df_rentals.zipcode.isin(common_zipcodes)
+            )
+
+            # Compute normalized measures
+            df_rentals = (
+                df_rentals.withColumn(
+                    "daily_price", df_rentals.rent * 12.0 / 365.0
+                )
+                .withColumn(
+                    "monthly_price_per_person",
+                    df_rentals.rent / df_rentals.capacity,
+                )
+                .withColumnRenamed("rent", "monthly_price")
+                .select([cname for cname in cols_list])
+            )
+
+            # Compute final results
+            self.output = (
+                df_airbnb.union(df_rentals)
+                .groupBy(["source", "zipcode"])
+                .agg(
+                    mean("daily_price").alias("avg_daily_price"),
+                    mean("monthly_price").alias("avg_monthly_price"),
+                    mean("monthly_price_per_person").alias(
+                        "avg_monthly_price_per_person"
+                    ),
+                )
+            )
             logger.info("Gold data processed successfully.")
             return self.output
         except Exception as e:
